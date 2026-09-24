95af0f82d637ba483ddff7e3a7f8201fb17e3f6a
D1ego-H3nriqu3 <dh6451013@gmail.com>
2026-09-24 09:51:41 -0300

    feat: adicionada função principal de controle de números e operações

--- calculadora.js
+++ calculadora.js
@@ -13,6 +13,35 @@ do{
     let resposta = (prompt("Escolha uma opção: ")).trim()
     switch(resposta){
         case("1"):
+            numerosOperacoes()
+            break
+        case("2"):
+            numerosOperacoes()
+            break
+        case("3"):
+            numerosOperacoes()
+            break
+        case("4"):
+            numerosOperacoes()
+            break
+        case("0"):
+
+            break
+        default:
+            console.log("\nDigite uma opção correspondente;")
+    }
+}while(indice === true)
+
+    function numerosOperacoes(resposta){
+        console.log("\n--Informando números--\n")
+        let num1 = Number(prompt("Primeiro, informe o primeiro número da operação: "))
+        let num2 = Number(prompt("Agora, informe o segundo número da operação: "))
+        if(isNaN(num1) || isNaN(num2)){
+            console.log("\nDigite apenas NÚMEROS!")
+            numerosOperacoes()
+        }else{
+            switch(resposta){
+        case("1"):
 
             break
         case("2"):
@@ -24,10 +53,8 @@ do{
         case("4"):
 
             break
-        case("0"):
-
-            break
         default:
-            console.log("\nDigite uma opção correspondente;")
+            console.log("Isso não deveria ocorrer...")
+            }
+        }
     }
-}while(indice === true)
