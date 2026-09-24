7afb9773fa4c24f8edc32b6dc30df99747e5bda2
D1ego-H3nriqu3 <dh6451013@gmail.com>
2026-09-24 09:33:55 -0300

    feat: adiciona o menu principal e sua verificação lógica

--- calculadora.js
+++ calculadora.js
@@ -1,2 +1,33 @@
 import PromptSync from "prompt-sync";
 const prompt = PromptSync()
+
+let indice = true
+
+do{
+    console.log(`\n=== Calculadora ===
+1. Soma
+2. Subtração
+3. Multiplicação
+4. Divisão
+0. Sair`)
+    let resposta = (prompt("Escolha uma opção: ")).trim()
+    switch(resposta){
+        case("1"):
+
+            break
+        case("2"):
+
+            break
+        case("3"):
+
+            break
+        case("4"):
+
+            break
+        case("0"):
+
+            break
+        default:
+            console.log("\nDigite uma opção correspondente;")
+    }
+}while(indice === true)
